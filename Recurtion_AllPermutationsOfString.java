e2300ace851e3a3e32f736935d8d234e43ddf5d0
SonarJayesh <jayeshsonar2824@gmail.com>
2024-09-02 18:30:35 +0530

    [ADDED]: JAYESH Java DSA Code for Print the All permutations of string using Recurtion

---
+++ Recurtion_AllPermutationsOfString.java
@@ -0,0 +1,19 @@
+public class Recurtion_AllPermutationsOfString {
+
+    public static void printPermutation(String str, String permutation){
+        if (str.length() == 0) {
+            System.out.println(permutation);
+            return;
+        }
+
+        for(int i=0; i<str.length(); i++){
+            char currChar = str.charAt(i);  
+            String newString = str.substring(0,i) + str.substring(i+1);
+            printPermutation(newString, permutation + currChar);
+        }
+    }
+    public static void main(String[] args) {
+        String str = "abc";
+        printPermutation(str, "");
+    }
+}

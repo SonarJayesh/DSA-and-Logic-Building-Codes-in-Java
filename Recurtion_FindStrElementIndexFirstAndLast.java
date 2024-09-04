1cede1416aa3dda180e4331b4079e1c6454b9e14
SonarJayesh <jayeshsonar2824@gmail.com>
2024-09-04 22:49:43 +0530

    [ADDED]: JAYESH Dsa and logical code for find String elements index first and last

---
+++ Recurtion_FindStrElementIndexFirstAndLast.java
@@ -0,0 +1,29 @@
+public class Recurtion_FindStrElementIndexFirstAndLast {
+    public static int first = -1;
+    public static int last = -1;
+
+    public static void findOccurence(String str, int index, char element){
+        if(index == str.length()){
+            System.out.println(first);
+            System.out.println(last);
+
+            return;
+        }
+
+        char currChar = str.charAt(index);
+        if(currChar == element){
+            if(first == -1){
+                first = index;
+            }else{
+                last = index;
+            }
+        }
+        findOccurence(str, index+1, element);
+    }
+
+    public static void main(String[] args) {
+        String str = "abaacdarfaah";
+        findOccurence(str , 0 , 'a');
+
+    }
+}

aef0e7f8dbf124569cdb794f3c7894eb486ca610
SonarJayesh <jayeshsonar2824@gmail.com>
2024-09-04 22:46:05 +0530

    [ADDED]: JAYESH java DSA and logical coode for print the Fibonaci series using recurtion

---
+++ Recurtion_Fibonaci.java
@@ -0,0 +1,23 @@
+public class Recurtion_Fibonaci {
+
+    public static void printFibo(int a , int b , int n){
+        
+        if (n == 0) {
+            return;
+        }
+
+        int c = a+b;
+        System.out.println(c);
+        printFibo(b, c, n-1);
+
+    }
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 1;
+        System.out.println(a);
+        System.out.println(b);
+        int n =10;
+        printFibo(a, b, n-2);
+
+    }
+}
